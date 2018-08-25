efdbc480faccf2bee90ae1946230e0d08c137fd4
BooeySays <BooeySays@Gmail.com>
2018-08-24 17:24:39 -0700

    removed line 5 (clear) and 8 (. /home/booeysays/mysc...)

--- cdandls.sh
+++ cdandls.sh
@@ -2,7 +2,8 @@
 
 CDANDLS(){
 cd "$@";
-clear;
+#clear;
+echo;
 ls;
-. $HOME/myscripts/files/whereami.sh;
+#. $HOME/myscripts/files/whereami.sh; # Removing this because I think it will look better if it's part of the PS1 prompt
 }
